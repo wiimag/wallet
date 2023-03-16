fbe2c0b66a01501c27471ee1fb381a19d7481219
Jonathan Schmidt <jschmidt42@gmail.com>
2023-03-16 11:02:20 -0400

    Update foundation_libs 1.6.3 Source: https://github.com/mjansson/foundation_lib/commit/7d90581dc602540a9d7e7f65543fc406f93ee758 From: https://github.com/Infineis/foundation_lib/commit/7dde6fdda05fea9c42f13e1a62d72da842242689

--- external/foundation/version.c
+++ external/foundation/version.c
@@ -1,7 +1,11 @@
+/* ****** AUTOMATICALLY GENERATED, DO NOT EDIT ******
+   This file is generated from the git describe command.
+   Run the configure script to regenerate this file */
+
 #include <foundation/version.h>
 #include <foundation/foundation.h>
 
 version_t
 foundation_version(void) {
-	return version_make(1, 6, 3, 0, 0x46ac4c1);
+	return version_make(1, 6, 2, 105, 0x7dde6fd);
 }
